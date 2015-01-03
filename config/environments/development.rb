c8a4119ffea8339fbb80308cd0394dbdd9d6c34a
Jon Roby <jonathan.roby1@gmail.com>
2015-01-03 01:33:44 -0500

    Implement Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Required for Heroku
+
+  config.action_mailer.default_url_options = { host: 'https://limitless-plains-6774.herokuapp.com' }
 end
